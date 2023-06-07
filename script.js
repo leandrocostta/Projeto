373bb01c338def0808744df399a58de881e321fb
leandrocostta <leandrocosttas@outlook.com>
2023-06-07 11:35:00 -0300

    toogle

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-function toggleMode () {
-const html= document.documentElement
-html.classList.toggle ("light")
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
 
-//pegar a tag img
-const img = document.querySelector("#profile img")
-
-if(html.classList.contains("light")){
-  img.setAttribute('src', 'avatar-light.png')
-}
-else {
-  img.setAttribute('src', 'Avatar.png')
-}
-
+  const img = document.querySelector("#profile img")
 }
